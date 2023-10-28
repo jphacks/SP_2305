8fb9f3802dd00c8be662304fd3ea6d0c421e9017
Utaho Akashi <akashi.uta.1212@gmail.com>
2023-10-28 23:06:06 +0900

    Add MonthButton

--- frontend/src/components/CalenderHeader.jsx
+++ frontend/src/components/CalenderHeader.jsx
@@ -4,7 +4,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import GlobalContext from "../context/GlobalContext";
 import { ButtonGroup, Button, IconButton } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const CalendarHeader = () => {
   const { monthIndex, setMonthIndex, setShowModalTabs, weekStartDay, setWeekStartDay } = useContext(GlobalContext);
@@ -21,6 +21,16 @@ export const CalendarHeader = () => {
     setMonthIndex(dayjs().month());
     setWeekStartDay(dayjs());
   };
+  const location = useLocation(); // 現在のURLを取得
+  const monthButtonStyle = {
+    colorScheme: location.pathname === '/' ? 'blue' : 'gray',
+    variant: 'solid',
+  };
+  const weekButtonStyle = {
+    colorScheme: location.pathname === '/week' ? 'blue' : 'gray',
+    variant: 'solid',
+  };
+
   return (
     <header className="px-4 py-2 flex items-center">
       <h1 className="mr-10 text-xl text-gray-500 fond-bold">Calendar</h1>
@@ -40,9 +50,14 @@ export const CalendarHeader = () => {
       <h2 className="ml-4 tgixt-xl text-gray-500 font-bold">
         {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
       </h2>
+      <Link to="/">
+        <Button {...monthButtonStyle}>
+          Month
+        </Button>
+      </Link>
       <Link to="/week">
-        <Button colorScheme='teal' variant='solid'>
-          WEEK
+        <Button>
+          Week
         </Button>
       </Link>
       <ButtonGroup
